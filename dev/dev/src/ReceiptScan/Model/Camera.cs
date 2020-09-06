1223fd12ca46c8abe0fb26085824a7dbb7df4958
CountrySideEngineer <deep-sleeper@nifty.com>
2020-09-06 15:42:08 +0900

    Update VisualStudio2019 solution and project to user camera. -Add VisualStudio2019 solution, project, and their source code files to use USB camera with OpenCv.  This application is by WPF, .NET Core 3.1.

---
+++ dev/dev/src/ReceiptScan/Model/Camera.cs
@@ -0,0 +1,84 @@
+﻿using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Text;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace ReceiptScan.Model
+{
+	/// <summary>
+	/// Camera object.
+	/// </summary>
+	public class Camera
+	{
+		protected VideoCapture capture;
+		protected Window window;
+
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public Camera()
+		{
+			this.capture = new VideoCapture();
+			capture.Open(0);
+		}
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="width">Width of camera image.</param>
+		/// <param name="height">Height of camera image</param>
+		/// <param name="fps">camera fps</param>
+		public Camera(int width, int height, int fps = 60)
+		{
+			this.capture = new VideoCapture();
+			capture.Open(0);
+			capture.FrameWidth = width;
+			capture.FrameHeight = height;
+			capture.Fps = fps;
+		}
+
+		/// <summary>
+		/// Read image from camera
+		/// </summary>
+		/// <returns>Image data read from camera in ImageSource object</returns>
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE0063:単純な 'using' ステートメントを使用する", Justification = "<保留中>")]
+		public ImageSource Read()
+		{
+			if (!this.capture.IsOpened())
+			{
+				return null;
+			}
+
+			var img = new Mat();
+			this.capture.Read(img);
+			if (img.Empty())
+			{
+				return null;
+			}
+
+			using (var memStream = new MemoryStream())
+			{
+				var bitMap = img.ToBitmap();
+				bitMap.Save(memStream, ImageFormat.Bmp);
+				memStream.Position = 0; //Set position to stream head.
+				var bitMapImage = new BitmapImage();
+				bitMapImage.BeginInit();
+				bitMapImage.CacheOption = BitmapCacheOption.OnLoad;
+				bitMapImage.CreateOptions = BitmapCreateOptions.None;
+				bitMapImage.StreamSource = memStream;   //The data is written into image...maybe.
+				bitMapImage.EndInit();
+				bitMapImage.Freeze();
+
+				return bitMapImage;
+			}
+		}
+	}
+}
---
+++ dev/dev/src/ReceiptScan/ViewModel/ReceiptScanViewModel.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+using Microsoft.VisualBasic;
+using OpenCvSharp;
+using OpenCvSharp.Extensions;
+using ReceiptScan.Command;
+using ReceiptScan.Model;
+
+namespace ReceiptScan.ViewModel
+{
+	/// <summary>
+	/// Main view model of ReceiptScan application.
+	/// </summary>
+	public class ReceiptScanViewModel : ViewModelBase
+	{
+		#region Private fields and constants
+		/// <summary>
+		/// Image field.
+		/// </summary>
+		protected ImageSource imgSource;
+
+		/// <summary>
+		/// Command to handle camera
+		/// </summary>
+		protected DelegateCommand cameraCommand;
+
+		/// <summary>
+		/// State of camera, running or stop.
+		/// </summary>
+		protected bool isCameraRunning;
+
+		/// <summary>
+		/// Content of button to start and stop camera.
+		/// </summary>
+		protected string buttonContent; 
+
+		/// <summary>
+		/// Timer to update camera view.
+		/// </summary>
+		protected DispatcherTimer cameraTimer;
+
+		/// <summary>
+		/// FPS of camera.
+		/// </summary>
+		protected int fps;
+		#endregion
+
+		#region Constructors and the finalizer
+		/// <summary>
+		/// Default constructor.
+		/// </summary>
+		public ReceiptScanViewModel()
+		{
+			this.fps = 30;
+
+			this.MyCamera = new Camera(1280, 720);
+
+			this.cameraTimer = new DispatcherTimer();
+			int interval = (int)Math.Round((decimal)1000 / fps);
+			cameraTimer.Interval = new TimeSpan(0, 0, 0, 0, interval);
+			cameraTimer.Tick += new EventHandler(this.TimerEvent);
+
+			this.buttonContent = "Start";
+			this.isCameraRunning = false;
+		}
+		#endregion
+
+		#region Public properties
+		/// <summary>
+		/// Image property.
+		/// </summary>
+		public ImageSource ImgSource
+		{
+			get
+			{
+				return this.imgSource;
+			}
+			set
+			{
+				this.imgSource = value;
+				this.RaisePropertyChanged(nameof(ImgSource));
+			}
+		}
+
+		/// <summary>
+		/// Camera object.
+		/// </summary>
+		public Camera MyCamera { get; set; }
+		#endregion
+
+		#region Method
+		/// <summary>
+		/// Command of camera.
+		/// </summary>
+		public DelegateCommand CamereCommand
+		{
+			get
+			{
+				if (null == this.cameraCommand)
+				{
+					this.cameraCommand = new DelegateCommand(this.CameraCommandExecute);
+				}
+				return cameraCommand;
+			}
+		}
+
+		/// <summary>
+		/// Proeprty of camera button content.
+		/// </summary>
+		public string ButtonContent
+		{
+			get
+			{
+				return this.buttonContent;
+			}
+			set
+			{
+				this.buttonContent = value;
+				this.RaisePropertyChanged(nameof(ButtonContent));
+			}
+		}
+
+		/// <summary>
+		/// Timer dispatcher of camera.
+		/// </summary>
+		/// <param name="sender">Not use</param>
+		/// <param name="e">Not use</param>
+		private void TimerEvent(object sender, EventArgs e)
+		{
+			this.ImgSource = this.MyCamera.Read();
+		}
+
+		/// <summary>
+		/// Execute camera command to start or stop.
+		/// </summary>
+		public void CameraCommandExecute()
+		{
+			if (this.isCameraRunning)
+			{
+				this.cameraTimer.Stop();
+				this.ButtonContent = "Start";
+				this.isCameraRunning = false;
+			}
+			else if (!this.isCameraRunning)
+			{
+				this.cameraTimer.Start();
+				this.ButtonContent = "Stop";
+				this.isCameraRunning = true;
+			}
+			else
+			{
+				//This code must be unreachable.
+			}
+		}
+		#endregion
+	}
+}
